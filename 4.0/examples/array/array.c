b8a88dc3af45a477f7ea66d511c4f1c92e9aae90
Leandro <leandrors@usp.br>
2018-05-10 16:06:22 -0300

    cleanup

---
+++ 4.0/examples/array/array.c
@@ -0,0 +1,49 @@
+// Multi-dimensional arrays
+// Expected result: 156
+
+#include <stdio.h>
+volatile int array[12] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+volatile int a1[12] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+volatile int a2[12] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+volatile int array2[12];
+
+int fct(int size) {
+    int result = 0;
+    int i;
+
+    loop4: for (int i = 0; i < 12; i++) {
+        array2[i] = a1[i]*a2[i-2];
+    }
+
+    loop5: for (i = 0; i < size; i++) {
+        result += array2[i];
+    }
+    return result;
+}
+
+int main() {
+    int result = 0;
+    int a, b, c;
+
+
+    loop1: for (a = 0; a < 2; a++) {
+        loop2: for (b = 0; b < 2; b++) {
+            result = array[a*2+b*2+c]/array[a*2+b*2+c];
+            loop3: for (c = 0; c < 3; c++) {
+                result += array[a*2+b*2+c]+array[a*3+b*2+c];
+            }
+        }
+    }
+
+
+
+    result += fct(12);
+
+    printf("Result: %d\n", result);
+    if (result == 156) {
+        printf("RESULT: PASS\n");
+    } else {
+        printf("RESULT: FAIL\n");
+    }
+    return result;
+}
